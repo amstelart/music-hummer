6ffe5799383c29e931c420c884d509c51c37c7d5
amstelart <amsteldroid@gmail.com>
2020-03-17 09:48:34 +0300

    dev-2

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,6 +1,74 @@
 // Если на проекте jQuery
 $( document ).ready(function() {
-    $('.main-slider').slick();
+
+    	// закрыть/открыть мобильное меню
+	var toggMnu = $(".toggle-mnu").click(function () {
+
+		// $(this).toggleClass("on");
+		// $(".toggle-mnu-2").toggleClass("on");
+		// $("body").toggleClass("fixed");
+		$(".hidden-mnu").toggleClass("active");
+		$("body, html").toggleClass("fixed");
+		return false;
+	});
+
+	$(document).mouseup(function (e) {
+		var container = $(".hidden-mnu.active");
+		if (container.has(e.target).length === 0) {
+			$(".toggle-mnu-1").removeClass("on");
+			// $("body").toggleClass("fixed");
+			$(".hidden-mnu").removeClass("active");
+			$("body, html").removeClass("fixed");
+		}
+	});
+
+	// /закрыть/открыть мобильное меню
+
+    // main-slider
+    $('.main-slider__carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.main-slider__nav',
+        responsive: [
+            {
+              breakpoint: 1199,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            }
+          ]
+        });
+    $('.main-slider__nav').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        asNavFor: '.main-slider__carousel',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+              breakpoint: 1199,
+              settings: {
+
+              }
+            }
+          ]
+    });
+    // end main-slider
+
+    // options-carousel
+    $('.options-carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+      });
+    // end options-carousel
 
     $('.rating.edit .star').hover(function() {
         var block=$(this).parents('.rating');
